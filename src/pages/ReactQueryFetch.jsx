32052bb4f3cc8297cd2d832d630c656145941fdb
Sham-sha <sham08092003@gmail.com>
2025-05-09 16:05:10 +0530

    react quary

--- src/pages/ReactQueryFetch.jsx
+++ src/pages/ReactQueryFetch.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+import { List, Typography, Spin, Alert } from "antd";
 
 function ReactQueryFetch() {
+  const fetchPost = () => {
+    return axios.get("http://localhost:3001/posts");
+  };
+
+  const { data, isLoading, error, isError , isFetching } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => fetchPost(),
+  });
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Spin tip="Loading..." size="large" />
+      </div>
+    );
+  }
+
+  console.log("hello",isFetching);
+  
+  if (isError) {
+    return (
+      <Alert
+        message="Error"
+        description={error.message}
+        type="error"
+        showIcon
+        style={{ marginTop: 50 }}
+      />
+    );
+  }
+
   return (
     <main>
-      <p>React Query Fetch</p>
+      <Typography.Title level={4}>This is fetch page</Typography.Title>
+      <List
+        bordered
+        dataSource={data?.data}
+        renderItem={(item) => (
+          <List.Item>
+            <Typography.Text>{item.title}</Typography.Text>
+          </List.Item>
+        )}
+      />
     </main>
-  )
+  );
 }
 
-export default ReactQueryFetch
+export default ReactQueryFetch;
