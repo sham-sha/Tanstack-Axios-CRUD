db3b28eadc362b5703ce45a17cdd8b2a1a5b62eb
sham-sha <sham08092003@gmail.com>
2025-06-21 22:51:19 +0530

    bug solved

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,12 @@ import ReactQueryFetch from "./pages/ReactQueryFetch";
 import ReactQueryFetchByClick from "./pages/ReactQueryFetchByClick";
 import ReactQueryByID from "./pages/ReactQueryByID";
 import PaginationQueries from "./pages/PaginationQueries";
-import InfiniteQueries from "./pages/infiniteQueries";
+import InfiniteQueries from "./pages/InfiniteQueries";
 import UseQueriesDemo from "./pages/useQueriesDemo";
 import { Result } from "antd";
 
 function App() {
   // Import the Ant Design Result component
-
   return (
     <>
       <Router>
